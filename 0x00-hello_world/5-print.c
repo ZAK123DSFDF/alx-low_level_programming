572afbd7e6def20659a97c4524bf65535eb1c350
ZAK123DSFDF <zekariyasberihun8@gmail.com>
2023-07-14 18:20:55 +0100

    print

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piece of art, \n");
+	return (0);
+}
